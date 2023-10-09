14606414508aa41289018d6a2da83d275d5a11fa
Jethrolopwus <lopwusjethro92@gmail.com>
2023-10-09 15:56:40 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a main with puts function
+ * Return:  o (sucess)
+ */
+int main(void)
+{
+puts("| "Programming is like building a multilingual puzzle");
+return (0);
+}
